4b1c897b6d5715f3cff6dd10aae473217c22a3f1
Prathik <pm6383760@gmail.com>
2026-06-10 11:05:25 +0530

    alternate solution for problem 1

---
+++ JavaScript/prob1.js
@@ -0,0 +1,13 @@
+let a = 8
+let b = 38
+let c = 70
+let d = 40
+
+let condition1 =((a+b >50) && (c+d  > 50))
+console.log("condition 1:",condition1)
+
+let condition2 =((a+b >100) && (c+d  > 100))
+console.log("condition 2:",condition2)
+
+let condition3 =((a+b <50) && (c+d < 50))
+console.log("condition 3:",condition3)
